9a5518c2b299f77c75f75efca1e388d75bbff6f5
Robin de Mourat <robin.demourat@gmail.com>
2021-04-26 15:53:13 +0200

    add linegraph data preparation

---
+++ public/data/unpivot4average.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const dsv = require('d3-dsv');
+const fileName = '4average_clusters_pivoted.csv';
+
+const MONTH_NAMES = [
+  'Janvier',
+  'Février',
+  'Mars',
+  'Avril',
+  'Mai',
+  'Juin',
+  'Juillet',
+  'Août',
+  'Septembre',
+  'Octobre',
+  'Novembre',
+  'Décembre'
+]
+
+const matrix = fs.readFileSync(fileName, 'utf8').split('\n').map(l => l.split(','))
+
+
+const firstRow = matrix[0];
+let dates = firstRow.slice(1).reduce((res, item, index) => {
+  const year = res.currentYear;
+  const monthNumeric = res.currentMonth;
+  const monthName = MONTH_NAMES[res.currentMonth];
+
+  return {
+    ...res,
+    results: [
+      ...res.results,
+      {
+        index,
+        monthNumeric,
+        monthName,
+        year,
+        fullName: `${monthName} ${year}`
+      }
+    ],
+    currentMonth: res.currentMonth < 11 ? res.currentMonth + 1 : 0,
+    currentYear: res.currentMonth < 11 ? res.currentYear : res.currentYear + 1,
+  };
+}, {
+  currentYear: 2015,
+  currentMonth: 0,
+  results: []
+});
+dates = dates.results;
+const unpivoted = matrix.slice(1).reduce((res, row) => {
+  const topic = row[0];
+  for (let i = 1 ; i < row.length ; i++) {
+    const month = firstRow[i].trim();
+    const value = +row[i].trim();
+    res.push({
+      topic,
+      month,
+      value,
+      ...dates[i - 1]
+    })
+  }
+  return res;
+}, [])
+
+const asStr = dsv.csvFormat(unpivoted);
+fs.writeFileSync('4average_clusters_unpivoted.csv', asStr, 'utf8')
