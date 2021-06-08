5f8a1748af2fc7a6d9f2f20b293e14252c0b36bb
Robin de Mourat <robin.demourat@gmail.com>
2021-06-08 09:45:24 +0200

    improve design

--- src/components/VisBlock/VisBlock.js
+++ src/components/VisBlock/VisBlock.js
@@ -1,10 +1,50 @@
 
 import cx from 'classnames';
 import React, {useContext} from 'react';
-import { PresentationContext} from '../../contexts'
+import { PresentationContext} from '../../contexts';
 
+const filterColorPalette = (colorVariable, colorPalette = {}, filters = [], filtersModeAnd) => {
+  if (!filters.length) {
+    return Object.entries(colorPalette);
+  }
+  return Object.entries(colorPalette)
+  .filter(([label, color]) => {
+    const matchesAtLeastOne = filters.find(({ attribute, value }) => {
+      if (attribute === colorVariable) {
+        return value === label;
+      }
+      return false;
+    })
+    return matchesAtLeastOne;
+  })
+}
 
-const VisBlock = React.forwardRef(({title, legend, id}, ref) => {
+const ColorLegend = ({colorPalette = []}) => {
+  return (
+    <ul className="ColorLegend">
+      {
+        colorPalette
+        .map(([label, color]) => (
+          <li>
+            <span className="color" style={{background: color}} />
+            <span className="label">{label}</span>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+const VisBlock = React.forwardRef(({
+  title, 
+  legend, 
+  id,
+  colorPalette,
+  nodeColorVariable,
+  filters,
+  filtersModeAnd,
+  colorVariable
+}, ref) => {
   const {
     activeVisualization,
     onBlockClick
@@ -28,6 +68,7 @@ const VisBlock = React.forwardRef(({title, legend, id}, ref) => {
         </p>
         : null
       }
+      <ColorLegend colorPalette={filterColorPalette(nodeColorVariable || colorVariable, colorPalette, filters, filtersModeAnd)} />
       
     </div>
   )
